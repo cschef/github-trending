3cab9f466862daf0c186734c56252e48a0283a2f
poetdp <poetdp@gmail.com>
2017-05-13 01:23:55 +0800

    修改git add 内容

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pyquery import PyQuery as pq
 
 
 def git_add_commit_push(date, filename):
-    cmd_git_add = 'git add {filename}'.format(filename=filename)
+    cmd_git_add = 'git add .'
     cmd_git_commit = 'git commit -m "{date}"'.format(date=date)
     cmd_git_push = 'git push -u origin master'
 
